5211bc2904d057e1be6505ae9d6b9422c8988cdd
Ashish Shukla <akshkgd@gmail.com>
2021-11-24 16:40:21 +0530

    new console added

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ function newQuote() {
     .then(function(data) {
       console.log(data);
       let quote = data[Math.floor(Math.random() * data.length)];
+      console.log(quote.text);
+      console.log(quote.author);
       document.getElementById("quote").innerHTML = quote.text;
       document.getElementById("author").innerHTML = quote.author;
     });
